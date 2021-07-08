1267ba2f386d6f4c1475e75c0f657f8292f641bd
klitztuch <info@mrlb.space>
2021-07-08 22:29:00 +0200

    cc

--- GetSongBpm.Net.Tests/GetSongBpmTest.cs
+++ GetSongBpm.Net.Tests/GetSongBpmTest.cs
@@ -1,13 +1,23 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
+using FluentAssertions;
+using GetSongBpm.Net.Models;
 using Microsoft.Extensions.Configuration;
+using Moq;
+using Moq.Protected;
 using NUnit.Framework;
 
 namespace GetSongBpm.Net.Tests
 {
     public class GetSongBpmTest
     {
-        private GetSongBpm _getSongBpm;
+        private string _apiKey;
+        private string _url;
         private IConfiguration Configuration { get; set; }
 
         [SetUp]
@@ -16,35 +26,140 @@ namespace GetSongBpm.Net.Tests
             var builder = new ConfigurationBuilder().AddUserSecrets<GetSongBpmTest>()
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
-            var apiKey = Configuration["TESTAPIKEY"];
-            _getSongBpm = new GetSongBpm(apiKey);
+            _apiKey = Configuration["TESTAPIKEY"];
+            _url = "https://api.getsongbpm.com";
         }
 
         [Test]
         public async Task GetArtistTest()
         {
-            const string id = "OMrJg";
-            var artist = await _getSongBpm.GetArtist(id);
-            Assert.IsNotNull(artist);
-            Assert.AreEqual(id, artist.Id);
+            // arrange
+            var expectedArtistResponse = new ArtistResponse
+            {
+                Artist = new Artist
+                {
+                    From = "GB",
+                    Genres = null,
+                    Id = "OMrJg",
+                    Img = "https://i.scdn.co/image/ab67616d0000b2731ff9ac286a38d5d85ce11ac3",
+                    Mbid = "f18ed420-f81a-43f6-83ff-9c021d5ef1c9",
+                    Name = "Sweet Little Band",
+                    Uri = "https://getsongbpm.com/artist/sweet-little-band/OMrJg"
+                }
+            };
+            var expectedArtist = expectedArtistResponse.Artist;
+            var mockHandler = CreateMockHandler(expectedArtistResponse, _url);
+            var httpClient = new HttpClient(mockHandler.Object);
+
+
+            var getSongBpm = new GetSongBpm(_apiKey, _url, httpClient);
+            // act
+            var actualArtist = await getSongBpm.GetArtist(expectedArtist.Id);
+            // assert
+            Assert.IsNotNull(actualArtist);
+            actualArtist.Should().BeEquivalentTo(expectedArtist);
         }
 
         [Test]
         public async Task GetSongTest()
         {
-            const string id = "6RNxAQ";
-            var song = await _getSongBpm.GetSong(id);
-            Assert.IsNotNull(song);
-            Assert.AreEqual(id, song.Id);
+            // arrange
+            var expectedSongResponse = new SongResponse
+            {
+                Song = new Song
+                {
+                    Artist = new Artist
+                    {
+                        From = "GB",
+                        Genres = null,
+                        Id = "OMrJg",
+                        Img = "https://i.scdn.co/image/ab67616d0000b2731ff9ac286a38d5d85ce11ac3",
+                        Mbid = "f18ed420-f81a-43f6-83ff-9c021d5ef1c9",
+                        Name = "Sweet Little Band",
+                        Uri = "https://getsongbpm.com/artist/sweet-little-band/OMrJg"
+                    },
+                    Id = "6RNxAQ",
+                    Tempo = "43",
+                    Title = "Blinding Lights",
+                    Uri = "https://getsongbpm.com/song/blinding-lights/57633B",
+                    KeyOf = "1",
+                    OpenKey = "1",
+                    TimeSig = "4/4"
+                }
+            };
+            var expectedSong = expectedSongResponse.Song;
+            var mockHandler = CreateMockHandler(expectedSongResponse, _url);
+            var httpClient = new HttpClient(mockHandler.Object);
+
+            var getSongBpm = new GetSongBpm(_apiKey, _url, httpClient);
+            // act
+            var actualSong = await getSongBpm.GetSong(expectedSong.Id);
+            // assert
+            Assert.IsNotNull(actualSong);
+            actualSong.Should().BeEquivalentTo(expectedSong);
         }
 
         [Test]
         public async Task GetSongsByTempoTest()
         {
+            // arrange
             const string bpm = "100";
-            var songsByTempo = await _getSongBpm.GetSongsByTempo(bpm);
-            Assert.IsNotNull(songsByTempo);
-            Assert.IsTrue(songsByTempo.All(o => o.TempoValue == bpm));
+            var expectedTempoResponse = new TempoResponse
+            {
+                Tempo = new List<Tempo>
+                {
+                    new()
+                    {
+                        Album = new Album(),
+                        Artist = new Artist(),
+                        SongId = "",
+                        SongTitle = "",
+                        SongUri = "",
+                        TempoValue = bpm
+                    },
+                    new()
+                    {
+                        Album = new Album(),
+                        Artist = new Artist(),
+                        SongId = "",
+                        SongTitle = "",
+                        SongUri = "",
+                        TempoValue = bpm
+                    }
+                }
+            };
+            var expectedTempos = expectedTempoResponse.Tempo;
+            var mockHandler = CreateMockHandler(expectedTempoResponse, _url);
+            var httpClient = new HttpClient(mockHandler.Object);
+
+            var getSongBpm = new GetSongBpm(_apiKey, _url, httpClient);
+            // act
+            var actualTempos = await getSongBpm.GetSongsByTempo(bpm);
+            // assert
+            Assert.IsNotNull(actualTempos);
+            actualTempos.Should().BeEquivalentTo(expectedTempos);
+        }
+
+        private static Mock<HttpMessageHandler> CreateMockHandler<T>(T responseObject,
+            string url)
+        {
+            var json = JsonSerializer.Serialize(responseObject);
+
+
+            var httpResponse = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+
+            var mockHandler = new Mock<HttpMessageHandler>();
+            mockHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                    ItExpr.Is<HttpRequestMessage>(message => message.Method == HttpMethod.Get
+                                                             && message.RequestUri.ToString().StartsWith(url)),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(httpResponse);
+            return mockHandler;
         }
     }
 }
